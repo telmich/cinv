4cd98172e9171c65aa38f57314cec155c3d2cd47
Nico Schottelius <nico@ikn.schottelius.org>
2009-11-12 10:22:09 +0100

    add template to add a host to a network
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ network-host-add.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+set -x
+
+name="$1"; shift
+type="$1"; shift
+location="$1"; shift
+network="$1"; shift
+
+# FIXME
+basedir="./"
+host_base="${basedir}/hosts/"
+host_dir="${host_base}/${name}"
+network_base="${basedir}/network/"
+network_dir="${network_base}/${network}"
+
+
+if [ ! -d "${network_dir}" ]; then
+   echo "No network configuration found for ${network}"
+   exit 1
+fi
+
+mkdir -p "${ddir}"
+echo "${type}" > "${ddir}/type"
+echo "${type}" > "${ddir}/location"
+
+

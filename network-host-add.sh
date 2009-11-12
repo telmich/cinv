6caf5e42405a6eebd8ab2d1e28407456f58b959e
Nico Schottelius <nico@ikn.schottelius.org>
2009-11-12 11:21:43 +0100

    add usage for network-host-add.sh
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- network-host-add.sh
+++ network-host-add.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
+
 name="$1"; shift
-type="$1"; shift
-location="$1"; shift
 network="$1"; shift
+ipn="$1"; shift
 
 # FIXME
 basedir="./"
@@ -14,6 +14,11 @@ host_base="${basedir}/hosts/"
 host_dir="${host_base}/${name}"
 network_base="${basedir}/network/"
 network_dir="${network_base}/${network}"
+
+usage()
+{
+   echo "name network [<ipn>|next-free]"
+}
 
 
 if [ ! -d "${network_dir}" ]; then

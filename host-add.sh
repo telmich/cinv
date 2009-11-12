556e5fc7d8bbae7b23c7ff60d4215a90eceeaad7
Nico Schottelius <nico@ikn.schottelius.org>
2009-11-12 11:22:01 +0100

    add header
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- host-add.sh
+++ host-add.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
+# 
+# 2009      Nico Schottelius (nico-sexy at schottelius.org)
+# 
+# This file is part of sexy.
+#
+# sexy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# sexy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with sexy. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Add a host (and do all the stuff depending on it)
+#
 
 set -e
 set -x
@@ -6,7 +27,10 @@ set -x
 name="$1"; shift
 type="$1"; shift
 location="$1"; shift
+
+# optional?
 network="$1"; shift
+address="..."
 
 # FIXME
 basedir="./"
@@ -16,10 +40,7 @@ network_base="${basedir}/network/"
 network_dir="${network_base}/${network}"
 
 
-if [ ! -d "${network_dir}" ]; then
-   echo "No network configuration found for ${network}"
-   exit 1
-fi
+network-host-add.sh "${name}" "${network}" "${address}"
 
 mkdir -p "${ddir}"
 echo "${type}" > "${ddir}/type"

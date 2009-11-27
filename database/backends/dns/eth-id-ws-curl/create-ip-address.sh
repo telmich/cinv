6fb11abdb344a63320d76383b19da1fdddcc20ff
Nico Schottelius <nico@ikn.schottelius.org>
2009-11-27 16:48:23 +0100

    FINISH! I can create a dns name!
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- database/backends/dns/eth-id-ws-curl/create-ip-address.sh
+++ database/backends/dns/eth-id-ws-curl/create-ip-address.sh
@@ -4,16 +4,16 @@ set -x
 
 [ $# -eq 2 ] || exit 1
 
-hostname="$1";shift
+fqdn="$1";shift
 ipv4a="$1";shift
 
 #cat << eof | curl  --insecure --basic --request POST --header "Content-Type: text/xml" --netrc https://www.komcenter.ethz.ch/ip/rest/nameToIP
 cat << eof | curl --verbose --basic --netrc --insecure --request POST \
---header "Content-Type: text/xml" --data - https://www.komcenter.ethz.ch/ip/rest/nameToIP
+--header "Content-Type: text/xml" --data @- https://www.komcenter.ethz.ch/ip/rest/nameToIP
 <insert>
   <nameToIP>
     <ip>$ipv4a</ip>
-    <fqName>$hostname</fqName>
+    <fqName>$fqdn</fqName>
     <forward>Y</forward>
     <reverse>Y</reverse>
     <ttl>600</ttl>

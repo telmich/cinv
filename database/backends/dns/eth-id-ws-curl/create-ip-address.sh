41e6ec6958dad1edf7aab17ee3d68fa557621c4b
Nico Schottelius <nico@ikn.schottelius.org>
2009-11-27 17:07:37 +0100

    Add header to create-ip-address.sh
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- database/backends/dns/eth-id-ws-curl/create-ip-address.sh
+++ database/backends/dns/eth-id-ws-curl/create-ip-address.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
-
-set -x
+# 
+# 2009      Nico Schottelius (nico-sexy at schottelius.org)
+# 
+# This file is part of sexy.
+#
+# sexy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# sexy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with sexy. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Setup an a-entry via curl to ETH ID Webservice
+#
 
 [ $# -eq 2 ] || exit 1
 
 fqdn="$1";shift
 ipv4a="$1";shift
 
-#cat << eof | curl  --insecure --basic --request POST --header "Content-Type: text/xml" --netrc https://www.komcenter.ethz.ch/ip/rest/nameToIP
 cat << eof | curl --verbose --basic --netrc --insecure --request POST \
 --header "Content-Type: text/xml" --data @- https://www.komcenter.ethz.ch/ip/rest/nameToIP
 <insert>
   <nameToIP>
-    <ip>$ipv4a</ip>
-    <fqName>$fqdn</fqName>
+    <ip>${ipv4a}</ip>
+    <fqName>${fqdn}</fqName>
     <forward>Y</forward>
     <reverse>Y</reverse>
     <ttl>600</ttl>

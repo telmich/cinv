1835cfcd3b994230244c0680be4141b6452c1e16
Nico Schottelius <nico@brief.schottelius.org>
2012-09-08 17:47:52 +0200

    add generic test suite loader
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

---
+++ lib/sexy/test/__main__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# 2012 Nico Schottelius (nico-sexy at schottelius.org)
+#
+# This file is part of sexy.
+#
+# sexy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sexy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with sexy. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import imp
+import os
+import sys
+import unittest
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+
+test_modules = []
+for possible_test in os.listdir(base_dir):
+    filename = "__init__.py"
+    mod_path = os.path.join(base_dir, possible_test, filename)
+
+    if os.path.isfile(mod_path):
+        test_modules.append(possible_test)
+
+suites = []
+for test_module in test_modules:
+    module_parameters = imp.find_module(test_module, [base_dir])
+    module = imp.load_module("sexy.test." + test_module, *module_parameters)
+
+    suite = unittest.defaultTestLoader.loadTestsFromModule(module)
+    suites.append(suite)
+
+all_suites = unittest.TestSuite(suites)
+unittest.TextTestRunner(verbosity=2).run(all_suites)
